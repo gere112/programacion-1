49fe9bb4320b216a4ed3ca503089e30878955809
Gerardo <82704817+gere112@users.noreply.github.com>
2022-05-10 13:14:47 -0300

    Add files via upload

---
+++ actividad_practica_arreglos_N°3/main.js
@@ -0,0 +1,25 @@
+let array = [28,19,89,78,22]
+
+ 
+let longitud=array.length
+console.log(longitud)
+
+
+
+
+function cambiar_pos(){
+
+    const val1 = parseInt(document.getElementById("pos_1").value)
+    const val2 = parseInt(document.getElementById("pos_2").value)
+
+    const elemento_1= array[val1]
+    const elemento_2 =array[val2]
+
+    console.log(elemento_1,elemento_2)
+
+        array[val1]= elemento_2
+        array[val2]= elemento_1
+
+        console.log(array)
+
+}
